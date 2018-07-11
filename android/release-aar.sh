2f5b069ff3ec2e9c11a89f4ccb5a491653221044
Edwin van Rooij <edwinvrooij@gmail.com>
2018-07-11 15:13:17 +0200

    attempt to include sources.jar (responsible for javadoc availability) in .aar file - note: only include in debug variant

--- android/release-aar.sh
+++ android/release-aar.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 bash gradlew :sdk:build # build new aar
-cp sdk/build/outputs/aar/sdk-* app/libs/ # copy all flavors to app/libs/
+cp sdk/build/outputs/aar/* app/libs/ # copy all flavors to app/libs/
